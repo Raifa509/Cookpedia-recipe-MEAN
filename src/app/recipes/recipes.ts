84d12aabeec206093c9d84bcc3a64e90d78dd12a
Raifa509 <fathimathulraifanp@gmail.com>
2025-11-29 14:07:00 +0400

    20/11/25

--- src/app/recipes/recipes.ts
+++ src/app/recipes/recipes.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
 import { ApiService } from '../services/api-service';
+import { Router } from '@angular/router';
+import { log } from 'console';
+
 
 @Component({
   selector: 'app-recipes',
@@ -11,7 +14,12 @@ import { ApiService } from '../services/api-service';
 })
 export class Recipes {
 allRecipes:any=[]
+cuisineArray:any=[]
+mealTypeArray:any=[]
+dummyAllRecipes:any=[]
+
   api=inject(ApiService)
+  router=inject(Router)
 
   ngOnInit(){
     this.getAllRecipes()
@@ -21,11 +29,37 @@ allRecipes:any=[]
     this.api.getAllRecipesAPI().subscribe(
       {
         next:(res:any)=>{
-          console.log(res);
+          // console.log(res);
           this.allRecipes=res
+          this.dummyAllRecipes=res
+          this.allRecipes.forEach((item:any)=>{
+            !this.cuisineArray.includes(item.cuisine) && this.cuisineArray.push(item.cuisine)
+          })
+          // console.log(this.cuisineArray);
+          
+          const dummyArray=this.allRecipes.map((item:any)=>item.mealType).flat(Infinity)
+          // console.log(dummyArray);
+          dummyArray.forEach((item:any)=>{
+            !this.mealTypeArray.includes(item) && this.mealTypeArray.push(item)
+          })
+          // console.log(this.mealTypeArray);
           
         }
       }
     )
+  }
+
+  //filter 
+  filterRecipe(key:string,value:string){
+    this.allRecipes=this.dummyAllRecipes.filter((item:any)=>item[key]==value)
+  }
+  //navigate to view page
+  navigateView(recipeId:string){
+    if(sessionStorage.getItem("token"))
+    {
+     this.router.navigateByUrl(`/recipes/${recipeId}/view`)
+    }else{
+      alert("Please login to get full access to our Recipe Collection!!!")
+    }
   }
 }
